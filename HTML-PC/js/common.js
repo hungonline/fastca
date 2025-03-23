e9a75a4e0db3a0c588aa4967ae7a0dd9d1822484
hungonline <hung91bn@gmail.com>
2025-03-23 20:46:11 +0700

    buid
    buid

--- HTML-PC/js/common.js
+++ HTML-PC/js/common.js
@@ -1,30 +1,30 @@
-(function ($) {
+(function($) {
     "use strict";
     var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
     var isMobile = {
-        Android: function () {
+        Android: function() {
             return navigator.userAgent.match(/Android/i);
         },
-        BlackBerry: function () {
+        BlackBerry: function() {
             return navigator.userAgent.match(/BlackBerry/i);
         },
-        iOS: function () {
+        iOS: function() {
             return navigator.userAgent.match(/iPhone|iPad|iPod/i);
         },
-        Opera: function () {
+        Opera: function() {
             return navigator.userAgent.match(/Opera Mini/i);
         },
-        Windows: function () {
+        Windows: function() {
             return navigator.userAgent.match(/IEMobile/i);
         },
-        any: function () {
+        any: function() {
             return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
         }
     }
 
     //backToTop
     function backToTop() {
-        $(window).scroll(function () {
+        $(window).scroll(function() {
             if ($(window).scrollTop() >= 200) {
                 $('#to_top').fadeIn();
                 $('.tp1-topnav').addClass('sticky');
@@ -35,13 +35,13 @@
         });
 
 
-        $("#to_top").click(function () {
+        $("#to_top").click(function() {
             $("html, body").animate({
                 scrollTop: 0
             });
             return false;
         });
-        $(window).scroll(function () {
+        $(window).scroll(function() {
             if ($(window).scrollTop() >= 60) {
                 $('.topnav ').addClass('sticky');
             } else {
@@ -59,7 +59,7 @@
     }
     //onCLick
     function onCLick() {
-        $('.auto_search_button').click(function () {
+        $('.auto_search_button').click(function() {
             $('.search-wrap input').focus();
             if (!$(this).hasClass('is-clicked')) {
                 $(this).addClass('is-clicked');
@@ -70,22 +70,22 @@
                 width: 'toggle'
             });
         });
-        $('#vibeji-ham').off('click').on('click', function () {
+        $('#vibeji-ham').off('click').on('click', function() {
             $(this).toggleClass('open');
             $('.main-menu').toggleClass('open');
             $('body').css('overflow', $(this).hasClass('open') ? 'hidden' : '')
         });
-        
-        $('.sub_menu').click(function () {
+
+        $('.sub_menu').click(function() {
             if ($(this).next('.level2').css('display') == 'none') {
                 $(this).html('-');
             } else {
                 $(this).html('+');
             };
-            $(this).next('.level2').slideToggle("slow", function () {});
+            $(this).next('.level2').slideToggle("slow", function() {});
         });
 
-        $(".tab-default >a").click(function (event) {
+        $(".tab-default >a").click(function(event) {
             $(".tab-default >a").removeClass("active");
             if (!$(this).hasClass("active")) {
                 $(this).addClass("active");
@@ -108,7 +108,7 @@
                 return;
             }
         });
-        $('.form-search .btn_reset').click(function () {
+        $('.form-search .btn_reset').click(function() {
             $('.form-search').removeClass("active");
         });
 
@@ -118,19 +118,18 @@
 
     //slide Gallery
     function swiper() {
-        var swiperBanner = new Swiper(".top-banner .swiper-container", {
+        var swiperFasca = new Swiper(".slide-fastca .swiper-container", {
             spaceBetween: 0,
             loop: false,
-            pagination: {
-                el: ".top-banner .swiper-pagination",
-                clickable: true,
+            navigation: {
+                nextEl: ".slide-fastca .swiper-button-next",
+                prevEl: ".slide-fastca .swiper-button-prev",
             },
             speed: 1000,
-            effect: 'fade',
-            autoplay: {
-                delay: 6000,
-                disableOnInteraction: false,
-            },
+            // autoplay: {
+            //     delay: 6000,
+            //     disableOnInteraction: false,
+            // },
         });
         var swiperTainang = new Swiper(".section-tainang .swiper-container", {
             slidesPerView: 4,
@@ -152,135 +151,11 @@
                 }
             }
         });
-        var swiperFeedback = new Swiper(".section-feedback .swiper-container", {
-            spaceBetween: 20,
-            slidesPerView: 3,
-            centeredSlides: true,
-            roundLengths: true,
-            loop: true,
-            loopAdditionalSlides: 30,
-            pagination: {
-                el: ".section-feedback .swiper-pagination",
-                clickable: true,
-            },
-            // autoplay: {
-            //     delay: 6000,
-            //     disableOnInteraction: false,
-            // },
-            breakpoints: {
-                991: {
-                    spaceBetween: 0,
-                },
-                767: {
-                    centeredSlides: false,
-                    slidesPerView: 2,
-                },
-                576: {
-                    centeredSlides: false,
-                    slidesPerView: 1.3,
-                }
-            }
-        });
-        
-        var swiperArticle = new Swiper(".slide-article .swiper-container", {
-            spaceBetween: 0,
-            loop: false,
-            speed: 1000,
-            effect: 'fade',
-            navigation: {
-                nextEl: ".slide-article .swiper-button-next",
-                prevEl: ".slide-article .swiper-button-prev",
-            },
-            autoplay: {
-                delay: 6000,
-                disableOnInteraction: false,
-            },
-        });
-        var swiperService = new Swiper(".list-service .swiper-container", {
-            slidesPerView: 3,
-            spaceBetween: 0,
-            simulateTouch: false,
-            navigation: {
-                nextEl: ".list-service .swiper-service-next",
-                prevEl: ".list-service .swiper-service-prev",
-            },
-            breakpoints: {
-                991: {
-                    slidesPerView: 2,
-                    spaceBetween: 24
-                },
-                414: {
-                    slidesPerView: 1.2,
-                    spaceBetween: 16,
-                }
-            }
-        });
-
-        var swiper = new Swiper(".slide-sevice .swiper-container", {
-            slidesPerView: 6,
-            spaceBetween: 24,
-            navigation: {
-                nextEl: ".slide-sevice .swiper-button-next",
-                prevEl: ".slide-sevice .swiper-button-prev",
-            },
-            breakpoints: {
-                1200: {
-                    slidesPerView: 5,
-                },
-                991: {
-                    slidesPerView: 4,
-                    spaceBetween: 16,
-                },
-                767: {
-                    slidesPerView: 3,
-                    spaceBetween: 16,
-                },
-                414: {
-                    slidesPerView: 2,
-                    spaceBetween: 16,
-                }
-            }
-        });
-
-        var swiperVPS = new Swiper(".list-vps .swiper-container", {
-            slidesPerView: 4,
-            spaceBetween: 25,
-            simulateTouch: false,
-            navigation: {
-                nextEl: ".list-vps .swiper-button-next",
-                prevEl: ".list-vps .swiper-button-prev",
-            },
-            breakpoints: {
-                991: {
-                    slidesPerView: 2,
-                    spaceBetween: 24
-                },
-                414: {
-                    slidesPerView: 1.2,
-                    spaceBetween: 16,
-                }
-            }
-        });
-
-        var swiperOne = new Swiper(".slide-one .swiper-container", {
-            spaceBetween: 0,
-            loop: false,
-            pagination: {
-                el: ".slide-one .swiper-pagination",
-                clickable: true,
-            },
-            speed: 1000,
-            autoplay: {
-                delay: 6000,
-                disableOnInteraction: false,
-            },
-        });
-
 
     }
 
 
-    $(function () {
+    $(function() {
         AOS.init({ disable: 'mobile' });
         backToTop();
         scrollBar();
